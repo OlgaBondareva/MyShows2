f3e1de06a4c45c14cca6c833815c584221771e41
Volha.Bondarava <vol.bondarava@gmail.com>
2018-12-20 11:51:23 +0300

    Fix pageobjects

--- spec/pageObject/loginPage.js
+++ spec/pageObject/loginPage.js
@@ -3,25 +3,28 @@ let navDrawer = require('./navigationDrawer')
 class loginPage extends navDrawer {
   constructor (driver) {
     super(driver)
-    this.titleSelector = 'android.widget.ScrollView > android.widget.LinearLayout > android.widget.TextView'
   }
 
-  get title () {return this.driver.element(this.titleSelector)}
+  get title () {return this.driver.$('//*[@text=\'my shows\']')}
 
-  get loginButton () {return this.driver.element('#ru.myshows.activity:id/login_button')}
+  get titleSelector () {return '//*[@text=\'my shows\']'}
 
-  get passField () {return this.driver.element('#ru.myshows.activity:id/password_field')}
+  get loginButton () {return this.driver.$('//*[@text=\'LOGIN\']')}
 
-  get loginField () {return this.driver.$('#ru.myshows.activity:id/login_field')}
+  get loginButtonLocator () {return '//*[@text=\'LOGIN\']'}
 
-  enterCredentialsAndSubmit (login, password) {
-    this.loginField.keys(login)
-    this.passField.keys(password)
-    this.loginButton.click()
+  get passField () {return this.driver.$('//*[@text=\'Password\']')}
+
+  get loginField () {return this.driver.$('//*[@text=\'Login\']')}
+
+  async enterCredentialsAndSubmit (login, password) {
+    await this.loginField.setValue(login)
+    await this.passField.setValue(password)
+    await this.driver.click(this.loginButtonLocator)
   }
 
-  async getTitle () {
-    return await this.driver.getText(this.titleSelector)
+  async isTitleVisible () {
+    return await this.driver.isVisible(this.titleSelector)
   }
 }
 
--- spec/pageObject/navigationDrawer.js
+++ spec/pageObject/navigationDrawer.js
@@ -1,41 +1,43 @@
 let basePage = require('./basePage')
 
-const wdio = require('wdio')
-
 class navigationDrawer extends basePage {
   constructor (driver) {
     super(driver)
-    this.navDrawerSelector = 'android.widget.ImageButton[content-desc="Open navigation drawer"]'
-    this.logoutAlertId = '#ru.myshows.activity:id/alertTitle'
   }
 
-  get navigationDrawerButton () {return this.driver.$(this.navDrawerSelector)}
+  get navigationDrawerButton () {return this.driver.$('//*[@content-desc="Open navigation drawer"]')}
 
-  get episodesButton () {return this.driver.$('*[text=\'Episodes\']')}
+  get navigationDrawerButtonSelector () {return '//*[@content-desc="Open navigation drawer"]'}
 
-  get showsButton () {return this.driver.$('*[text=\'Shows\']')}
+  get episodesButton () {return this.driver.$('//*[@text=\'Episodes\']')}
 
-  get newsButton () {return this.driver.$('*[text=\'News\']')}
+  get episodesButtonSelector () {return '//*[@text=\'Episodes\']'}
 
-  get settingsButton () {return this.driver.$('*[text=\'Settings\']')}
+  get showsButton () {return this.driver.$('//*[@text=\'Shows\']')}
 
-  get hideAdvertisingButton () {return this.driver.$('*[text=\'Hide advertising\']')}
+  get showsButtonSelector () {return '//*[@text=\'Shows\']'}
 
-  get logoutButton () {return this.driver.$('*[text=\'Logout\']')}
+  get newsButton () {return this.driver.$('//*[@text=\'News\']')}
+
+  get settingsButton () {return this.driver.$('//*[@text=\'Settings\']')}
+
+  get hideAdvertisingButton () {return this.driver.$('//*[@text=\'Hide advertising\']')}
+
+  get logoutButton () {return this.driver.$('//*[@text=\'Logout\']')}
 
   async isLoggedIn () {
-    await this.driver.waitForElementByXPath(this.navDrawerSelector, 5000)
-    return this.navigationDrawerButton.isVisible()
+    await this.driver.waitForVisible(this.navigationDrawerButtonSelector, 5000)
+    return this.driver.isVisible(this.navigationDrawerButtonSelector)
   }
 
   async openEpisodes () {
-    await this.navigationDrawerButton.click()
-    await this.episodesButton.click()
+    await this.driver.click(this.navigationDrawerButtonSelector)
+    await this.driver.click(this.episodesButtonSelector)
   }
 
   async openShows () {
-    await this.navigationDrawerButton.click()
-    await this.showsButton.click()
+    await this.driver.click(this.navigationDrawerButtonSelector)
+    await this.driver.click(this.showsButtonSelector)
   }
 }
 
