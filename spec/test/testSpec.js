e7223b1a7786ec0d4a603f873f33a7cc0c1fa45f
Volha.Bondarava <vol.bondarava@gmail.com>
2018-12-20 11:52:03 +0300

    Add some tesing route

--- spec/test/testSpec.js
+++ spec/test/testSpec.js
@@ -2,20 +2,29 @@ const webdriver = require('webdriverio')
 let caps = require('../helpers/desiredCapabilities').android27
 let loginPage = require('../pageObject/loginPage')
 let driver = webdriver.remote(caps)
+let credentials = require('../helpers/credentials')
 
 describe('App MyShows ', () => {
   let login
 
   beforeAll(async () => {
-    await driver.init()
+    await driver.pause(5000)
+    await driver.init().timeouts('implicit', 10000)
   })
 
   afterAll(async () => {
     await driver.end()
   })
 
-  it('should ', async () => {
+  it ('should have visible "MyShows" title on login page', async () => {
     login = await new loginPage(driver)
-    await login.enterCredentialsAndSubmit('test_test_test', '123456')
+    let isTitleVisible = await login.isTitleVisible()
+    expect(isTitleVisible).toBeTrue()
+  })
+
+  it('should login with right credentials', async () => {
+    await login.enterCredentialsAndSubmit(credentials.login, credentials.pass)
+    let isLoggedIn = await login.isLoggedIn()
+    expect(isLoggedIn).toBeTrue()
   })
 })
